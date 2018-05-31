07add940ba169d198f0ddaeffa8fb72b706a1163
Nikita Shilnikov <fg@flashgordon.ru>
2018-05-31 20:58:59 +0300

    Add operation for creating new customer

---
+++ app/operations/create_customer.rb
@@ -0,0 +1,36 @@
+require 'dry/validation'
+require 'dry/monads/result'
+require 'dry/monads/do/all'
+
+Dry::Validation.load_extensions(:monads)
+
+module Ledger
+  module Operations
+    class CreateCustomer
+      include Dry::Monads::Result::Mixin
+      include Dry::Monads::Do::All
+
+      Schema = Dry::Validation.JSON do
+        configure do
+          config.type_specs = true
+        end
+
+        required(:name, :string).filled(:str?)
+      end
+
+      attr_reader :repo
+
+      def initialize(repo:)
+        @repo = repo
+      end
+
+      def call(account, params)
+        values = yield Schema.(params)
+
+        customer = repo.create(account_id: account.id, **values)
+
+        Success(customer)
+      end
+    end
+  end
+end
---
+++ spec/unit/operations/create_customer_spec.rb
@@ -0,0 +1,19 @@
+require 'dry/monads/result'
+require 'operations/create_customer'
+
+RSpec.describe Ledger::Operations::CreateCustomer do
+  include Dry::Monads::Result::Mixin
+
+  let(:account) { Factory[:account] }
+  let(:customer) { Factory.structs[:customer, account: account] }
+
+  subject(:operation) { described_class.new(repo: double(create: customer)) }
+
+  it 'creates a customer' do
+    params = {
+      'name' => customer.name
+    }
+
+    expect(operation.(account, params)).to eql(Success(customer))
+  end
+end
