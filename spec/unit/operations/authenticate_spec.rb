82a66ade6b006c993b3407f020b60cd2341ae634
Nikita Shilnikov <fg@flashgordon.ru>
2018-06-02 15:36:18 +0300

    Cover unsuccessful authentication

--- spec/unit/operations/authenticate_spec.rb
+++ spec/unit/operations/authenticate_spec.rb
@@ -6,17 +6,25 @@ RSpec.describe Ledger::Operations::Authenticate do
 
   let(:hash_key) { Ledger::App['lib.hash_api_key'] }
 
-  let(:subject) { described_class.new(repo: repo) }
+  subject(:authenticate) { described_class.new(repo: repo) }
+
+  let(:api_key) { SecureRandom.uuid }
+
+  let(:account) { Factory.structs[:account, api_key: hash_key.(api_key)] }
 
   context 'successful authentication' do
-    let(:api_key) { SecureRandom.uuid }
-
-    let(:account) { Factory.structs[:account, api_key: hash_key.(api_key)] }
-
     let(:repo) { double(find_by_api_key: account) }
 
     it 'succeeds' do
-      expect(subject.(api_key)).to eql(Success(account))
+      expect(authenticate.(api_key)).to eql(Success(account))
+    end
+  end
+
+  context 'unsuccessful authentication' do
+    let(:repo) { double(find_by_api_key: nil) }
+
+    it 'fails' do
+      expect(authenticate.('123')).to eql(Failure(:account_not_found))
     end
   end
 end
