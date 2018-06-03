22af4a8a7d2824212395ac4490d5e326ec65b8dc
Nikita Shilnikov <fg@flashgordon.ru>
2018-06-03 20:15:19 +0300

    Add account locking

--- app/ledger/repos/account_repo.rb
+++ app/ledger/repos/account_repo.rb
@@ -7,11 +7,25 @@ module Ledger
       include ArgsImport['persistence.rom']
 
       def balance(account_id)
-        accounts.with_balance.by_pk(account_id).one!
+        balances.by_pk(account_id).one!
+      end
+
+      def lock(account_id)
+        transaction do
+          account = balances.lock.by_pk(account_id).one!
+          yield(account)
+          nil
+        end
       end
 
       def find_by_api_key(key)
         accounts.by_api_key(key).one
+      end
+
+      private
+
+      def balances
+        accounts.with_balance
       end
     end
   end
--- spec/unit/repos/account_repo_spec.rb
+++ spec/unit/repos/account_repo_spec.rb
@@ -4,17 +4,37 @@ RSpec.describe Ledger::Repos::AccountRepo, :db do
   subject(:repo) { described_class.new(rom) }
 
   let(:account) { Factory[:account] }
+
   let(:customer) { Factory[:customer, account: account] }
 
+  def create_charge
+    Factory[:charge, customer: Factory[:customer, account: account]]
+  end
+
+  def create_payout
+    Factory[:payout, account: account]
+  end
+
   describe '#balance' do
-    let!(:charges) { 3.times.map { Factory[:charge, customer: Factory[:customer, account: account]] } }
-    let!(:payouts) { 3.times.map { Factory[:payout, account: account] } }
+    let!(:charges) { 3.times.map { create_charge } }
+    let!(:payouts) { 3.times.map { create_payout } }
 
     it 'adds chargs and subtracts payouts' do
       account_with_balance = repo.balance(account.id)
 
       expect(account_with_balance.balance).
         to eql(charges.sum(&:amount) - payouts.sum(&:amount))
+    end
+  end
+
+  describe 'lock' do
+    let!(:charge) { create_charge }
+
+    it 'yields a block and passes balance' do
+      account_with_balance = nil
+      repo.lock(account.id) { |account| account_with_balance = account }
+
+      expect(account_with_balance.balance).to eql(charge.amount)
     end
   end
 end
