684adca358907133fde8fba235a9fd1fe55ef36a
Nikita Shilnikov <fg@flashgordon.ru>
2018-06-07 23:06:23 +0300

    Cover failed auth with spec

--- app/ledger/lib/api_authentication.rb
+++ app/ledger/lib/api_authentication.rb
@@ -12,15 +12,21 @@ module Ledger
                 'lib.basic_auth'
               ]
 
+      # dry-auto_inject is compatible with existing constructor
       def initialize(app, deps)
+        # we pass dependencies and assign them in `super`
         super(deps)
         @app = app
       end
 
       def call(env)
+        # get API key from request
         basic_auth.(env).
+          # try to authenticate
           bind(authenticate).
+          # either this line works
           fmap { |account| @app.(env.merge(ACCOUNT_KEY => account)) }.
+          # or this one
           value_or(UNAUTHORIZED)
       end
     end
--- spec/unit/app/lib/api_authentication_spec.rb
+++ spec/unit/app/lib/api_authentication_spec.rb
@@ -17,21 +17,37 @@ RSpec.describe Ledger::Lib::APIAuthentication, :api do
 
   let(:account) { Factory.structs[:account] }
 
-  let(:authenticate) do
-    proc do |key|
-      expect(key).to eql(api_key)
-      Success(account)
+  subject(:middleware) { described_class.new(app, authenticate: authenticate) }
+
+  describe 'success' do
+    let(:authenticate) do
+      proc do |key|
+        expect(key).to eql(api_key)
+        Success(account)
+      end
+    end
+
+    it 'searches account by key and adds it to env' do
+      env = make_auth_headers(api_key)
+      middleware.(env)
+
+      updated_env = requests[0]
+
+      expect(updated_env).to include('ledger.account' => account)
     end
   end
 
-  subject(:middleware) { described_class.new(app, authenticate: authenticate) }
+  describe 'failure' do
+    let(:authenticate) do
+      proc { Failure(:account_not_found) }
+    end
 
-  it 'searches account by key and adds it to env' do
-    env = make_auth_headers(api_key)
-    middleware.(env)
+    it 'return 401 back' do
+      env = make_auth_headers(api_key)
+      status, * = middleware.(env)
 
-    updated_env = requests[0]
-
-    expect(updated_env).to include('ledger.account' => account)
+      expect(status).to eql(401)
+      expect(requests).to be_empty
+    end
   end
 end
